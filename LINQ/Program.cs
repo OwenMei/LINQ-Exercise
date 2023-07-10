732245bcd775e8eb26904c3e300266c6ef82774c
owen mei <owenzackmei@gmail.com>
2023-07-10 16:21:26 -0400

    init commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Halo", "COD", "WOW", "DOTA", "Valorant", "League of Legends", "Minecraft", "Overwatch"};
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
         }
     }
 }
